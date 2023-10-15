cd8c49b6bbdece032f55c7204ceb7519602e5d56
ZTStew <zt.stewart.5@gmail.com>
2023-10-15 04:56:10 -0400

    Current state, does not work

--- trim.py
+++ trim.py
@@ -1,7 +1,8 @@
+import os
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-input_file = "test.mp4"
-output_file = "output.mp4"
+input_file = "./test.mp4"
+output_file = "./output.mp4"
 start_time = 5
 end_time = None  # None to keep the rest of the video
 
