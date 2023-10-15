a5ec2be6f1fe73b4e80c6a24cb583eb19a204b40
ZTStew <zt.stewart.5@gmail.com>
2023-10-15 04:45:58 -0400

    Testing .gitignore

---
+++ trim.py
@@ -0,0 +1,8 @@
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+
+input_file = "test.mp4"
+output_file = "output.mp4"
+start_time = 5
+end_time = None  # None to keep the rest of the video
+
+ffmpeg_extract_subclip(input_file, start_time, end_time, targetname=output_file)
