f1f3874447b77f79458eef424afba61676784379
ZTStew <zt.stewart.5@gmail.com>
2023-10-20 22:40:21 -0400

    Refactored to allow for trimming to apply to end of videos as well

--- trim.py
+++ trim.py
@@ -1,6 +1,5 @@
 """
 Update Ideas:
-  add in the option to remove from the end rather than the begining
   add in the ability to handle command-line options
 """
 
@@ -15,26 +14,28 @@ if len(sys.argv) > 1:
   # Prints how to use program into command line
   if sys.argv[1].lower() == "help":
     print("Arguments:\n  Trim Amount (Seconds): Int\n  Trim Direction [s/e]: String")
-    print("Trim Amount: The number of seconds you would like to trim. Default is: 5 seconds")
-    print("Trim Direction: Determines if the trim will be applied to the start of the video [s] or the end of the video [e]. Default is start of the video")
+    print("    Trim Amount: The number of seconds you would like to trim. Default: 5 seconds")
+    print("    Trim Direction: Determines if the trim will be applied to the [s]tart of the video or the [e]nd of the video. Default: Start of the video")
 
     help_flag = True
 
 if not help_flag:
   directory = "./"
   input_files = glob.glob(f"{directory}/*.mp4")
-  start_time = 5
+  trim_time = 5
   # False -> Trim start of video
   # True -> Trim end of video
   trim_direction = False
 
-  # assigns start_time value
+  # assigns trim_time value
   if len(sys.argv) > 1:
-    start_time = int(sys.argv[1])
+    trim_time = int(sys.argv[1])
 
   # assigns trim_direction value
   if len(sys.argv) > 2:
-    trim_direction = bool(sys.argv[2])
+    if "e" in sys.argv[2]:
+      trim_direction = True
+
 
   # Creates output folder to store processed .mp4 files
   check = os.path.exists("./output")
@@ -45,8 +46,15 @@ if not help_flag:
   def get_length(input_video):
       clip = VideoFileClip(input_video)
       return clip.duration
-
+  
   # Itterates through all .mp4 files found in given directory
   for input_file in input_files:
     file_name = os.path.basename(input_file)
-    ffmpeg_extract_subclip(input_file, start_time, get_length(input_file), targetname="./output/"+os.path.splitext(file_name)[0]+".mp4")
+
+    # Trims video from the start
+    if not trim_direction:
+        ffmpeg_extract_subclip(input_file, trim_time, get_length(input_file), targetname="./output/"+os.path.splitext(file_name)[0]+".mp4")
+
+    # Trims video from the end
+    if trim_direction:
+      ffmpeg_extract_subclip(input_file, 0, get_length(input_file) - trim_time, targetname="./output/"+os.path.splitext(file_name)[0]+".mp4")
