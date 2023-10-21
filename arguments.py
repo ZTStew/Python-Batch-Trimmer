42458ff77d19c085a6984934f82a007080454d5c
ZTStew <zt.stewart.5@gmail.com>
2023-10-21 17:15:15 -0400

    Refactored arguments to seperate object

--- arguments.py
+++ arguments.py
@@ -3,28 +3,48 @@ Object handles command line arguments, stores them together for reference
 
 """
 
-import os, sys, glob
+import sys, glob
 
 class Arguments:
   def __init__(self, args):
-  # def __init__(self, directory = "./", start_time = 5, trim_direction = False):
-
+  # def __init__(self, directory = "./", trim_time = 5, trim_direction = False):
+    # Contains command line arguments given by user
     self.args = args
-    # self.directory = directory
-    # self.input_files = glob.glob(f"{self.directory}/*.mp4")
-    self.start_time = 5
+
+    # Checks if the first argument in 'args' is "help"
+    self.help_check()
+
+    # Time user wants to trim off videos
+    self.trim_time = 5
     # False -> Trim start of video
     # True -> Trim end of video
     self.trim_direction = False
+    # Video format to search for to trim
+    self.file_type = ".mp4"
+    # Identifies the directory containing files user wants to trim
+    self.directory = "./"
+    # Collects all files inside of the given directory to trim
+    self.input_files = glob.glob(f"{self.directory}/*" + self.file_type)
 
-    self.help_check()
+    self.assign_args()
 
   # checks if the user typed in "help" as an argument. If they did, prints help text and exits program
   def help_check(self):
     if len(self.args) > 1:
       if self.args[1].lower() == "help":
-        print("Arguments:\n  Trim Amount (Seconds): Int\n  Trim Direction [s/e]: String")
-        print("    Trim Amount: The number of seconds you would like to trim. Default: 5 seconds")
-        print("    Trim Direction: Determines if the trim will be applied to the [s]tart of the video or the [e]nd of the video. Default: Start of the video")
+        print("Arguments:")
+        print("    Trim Amount (Seconds): Int -> The number of seconds you would like to trim. Default: 5 seconds")
+        print("    Trim Direction [s/e]: String -> Determines if the trim will be applied to the [s]tart of the video or the [e]nd of the video. Default: Start of the video")
         
+        # Program should not run its main function if 'help' argument is found
         sys.exit()
+
+  def assign_args(self):
+    # assigns trim_time value
+    if len(self.args) > 1:
+      self.trim_time = int(self.args[1])
+
+    # assigns trim_direction value
+    if len(self.args) > 2:
+      if "e" in self.args[2].lower():
+        self.trim_direction = True
--- trim.py
+++ trim.py
@@ -3,29 +3,13 @@ Update Ideas:
   add in the ability to handle command-line options
 """
 
-import os, glob, sys
+import os, sys
 from moviepy.editor import VideoFileClip
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from arguments import Arguments
 
 # handles all argument informaiton given when program is executed
 arg = Arguments(sys.argv)
-
-directory = "./"
-input_files = glob.glob(f"{directory}/*.mp4")
-trim_time = 5
-# False -> Trim start of video
-# True -> Trim end of video
-trim_direction = False
-
-# assigns trim_time value
-if len(sys.argv) > 1:
-  trim_time = int(sys.argv[1])
-
-# assigns trim_direction value
-if len(sys.argv) > 2:
-  if "e" in sys.argv[2]:
-    trim_direction = True
 
 # Creates output folder to store processed .mp4 files
 check = os.path.exists("./output")
@@ -38,13 +22,13 @@ def get_length(input_video):
     return clip.duration
 
 # Itterates through all .mp4 files found in given directory
-for input_file in input_files:
+for input_file in arg.input_files:
   file_name = os.path.basename(input_file)
 
   # Trims video from the start
-  if not trim_direction:
-      ffmpeg_extract_subclip(input_file, trim_time, get_length(input_file), targetname="./output/"+os.path.splitext(file_name)[0]+".mp4")
+  if not arg.trim_direction:
+      ffmpeg_extract_subclip(input_file, arg.trim_time, get_length(input_file), targetname="./output/"+os.path.splitext(file_name)[0]+".mp4")
 
   # Trims video from the end
-  if trim_direction:
-    ffmpeg_extract_subclip(input_file, 0, get_length(input_file) - trim_time, targetname="./output/"+os.path.splitext(file_name)[0]+".mp4")
+  if arg.trim_direction:
+    ffmpeg_extract_subclip(input_file, 0, get_length(input_file) - arg.trim_time, targetname="./output/"+os.path.splitext(file_name)[0]+".mp4")
